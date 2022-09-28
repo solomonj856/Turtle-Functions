58fac389af93cce70d075d93c476ab5f1feaabeb
solomonj856 <61333117+solomonj856@users.noreply.github.com>
2022-09-27 19:32:14 -0700

    Update turtleshapes.py

--- turtleshapes.py
+++ turtleshapes.py
@@ -38,24 +38,4 @@ def pentagon(x, y):
         turtle.left(72)
 
 
-def square(x, y):
-    #turtle.clear()
-    turtle.color('green', 'red')
-     
-    turtle.goto(x ,y)
 
-    turtle.pd()
-
-    
-    for i in range(4):
-        turtle.forward(250)
-        turtle.right(90)
-
-    
-    turtle.right(45)
-    turtle.forward(350)
-    turtle.left(90)
-    turtle.pu()
-    turtle.goto(x , y - 250)
-    turtle.pd()
-    turtle.forward(350)
